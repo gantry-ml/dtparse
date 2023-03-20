07eb396a556ed240b3ebfa1a86d838a111998829
Hao Chen <chenh1987@gmail.com>
2023-03-19 23:37:49 -0700

    Fix user test issues

--- test_dtparse.py
+++ test_dtparse.py
@@ -5,20 +5,14 @@ import gantry.dataset as gdataset
 from gantry.llm import EvaluationRun
 
 
-
-
 def test_model():
     model = make_model(BAD_PROMPT)
     dataset = gdataset.sync_dataset_data(
         "eval_set.json",
     )
     run = EvaluationRun(dataset)
-    with run.start():
+    with run.start(tag="stable"):
         for input in run.inputs:
             model(**input)
 
     assert run.metrics["exact_match"] == 1.0
-
-
-
-
