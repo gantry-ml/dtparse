c658b5379e4a1bddc8cf57b2ff9e9132dc289d36
Abhishyant Khare <abhishyant@gmail.com>
2023-03-21 13:35:14 -0400

    improvement: improve demo UX
    1. Add a docstring to `test_model` to describe what it's doing in more
       detail.
    2. Add a README to explain what this application does.
    3. Make it clearer in the test what line of code to change as part of
       the demo.

--- test_dtparse.py
+++ test_dtparse.py
@@ -4,14 +4,31 @@ from .prompts import GOOD_PROMPT, BAD_PROMPT
 import gantry.dataset as gdataset
 from gantry.llm import EvaluationRun
 
+# TODO: Change this to GOOD_PROMPT to see the test pass.
+PROMPT = BAD_PROMPT
+
+tag = "bug" if PROMPT == BAD_PROMPT else "fix"
+
 
 def test_model():
-    model = make_model(BAD_PROMPT)
+    """
+    A simple test to make sure the model is working as expected. The 
+    test works as follows:
+    
+    1. Initialize the model with a prompt template.
+    2. Load a dataset of test inputs from a json file. 
+    3. Initialize a run with the dataset.
+    4. Start the run. When we start the run, all OpenAI SDK calls
+    will be tracked, and inputs & outputs will be logged locally.
+    5. Iterate over the inputs and run the model on each input.
+    6. Assert that the run metrics are as expected.
+    """
+    model = make_model(PROMPT)
     dataset = gdataset.sync_dataset_data(
         "eval_set.json",
     )
     run = EvaluationRun(dataset)
-    with run.start(tag="stable"):
+    with run.start(tag=tag):
         for input in run.inputs:
             model(**input)
 
